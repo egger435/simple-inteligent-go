0844a8b6e020f679c2bcf9e13fbfc375d23b80e1
Egger <2931291681@qq.com>
2026-02-27 16:47:18 +0800

    修复了劫争导致的问题

--- src/go_play.py
+++ src/go_play.py
@@ -55,11 +55,11 @@ class KataGoEngine:
     def restart(self):
         self.close()
         self._start_engine()
-        print('重启KataGo')
+        print('重启KataGo...')
     
     def close(self):
         if self.process:
-            print('关闭KataGo')
+            print('关闭KataGo...')
             self.process.terminate()
             self.process.wait()
     
@@ -91,8 +91,13 @@ class KataGoEngine:
             line = self.process.stdout.readline()
             if not line:
                 continue
-
-            print(f'[KataGo] {line.strip()}')
+            
+            kata_message = line.strip()
+            print(f'[KataGo] {kata_message}')
+            if ('error' or 'Error' in kata_message) and 'rawStWrError' not in kata_message:
+                print(f"KataGo返回错误: {kata_message}")
+                self.restart()
+                return -2
             try:
                 resp = json.loads(line)
                 if 'error' in resp:
@@ -113,18 +118,19 @@ class KataGoEngine:
         # 解析结果
         player = 'b' if len(moves) % 2 == 0 else 'w'  # 下一个行棋方
         winrate = response['rootInfo']['winrate']
-        print(f'行棋方: {player} | 胜率: {100*winrate:.2f}%\n')
+        print(f'我方胜率: {100*(1-winrate):.2f}%\n')
 
         return winrate if root_player == player else (1-winrate)
 
 class TreeNode:
     '''MiniMax 树节点'''
-    def __init__(self, board_state, cur_player:str, deepth=0, 
+    def __init__(self, board_state, cur_player:str, ko_pos = None, deepth=0, 
                  parent=None, by_move=None, is_root=False, root_moves=None):
-        self.board_state = board_state.copy()  # 当前棋盘状态
+        self.board_state: boards.Board = board_state.copy()  # 当前棋盘状态
         self.cur_player  = cur_player          # 当前落子方
+        self.ko_pos      = ko_pos              # 劫争位
         self.deepth      = deepth              # 节点深度
-        self.parent: TreeNode = parent              # 父节点
+        self.parent: TreeNode = parent         # 父节点
         self.by_move     = by_move             # 通过何种落子到达该节点
         self.children    = []                  # 子节点列表
         self.value       = None                # 价值
@@ -135,8 +141,6 @@ class TreeNode:
 
         # 叶节点属性
         self.moves       = None                # 到达这个叶节点的所有落子序列
-        
-    
     
     def is_leaf(self):
         return self.deepth >= MAX_SEARCH_DEEPTH
@@ -162,22 +166,60 @@ class TreeNode:
     
 class MinimaxMCR:
     '''Minimax搜索树和蒙特卡洛推演算法实现类'''
-    def __init__(self, steps_list, root_state: boards.Board, root_player: chr, curstep: int):
+    def __init__(self, steps_list,  # 落子序列
+                 root_state: boards.Board, 
+                 root_player: chr, 
+                 curstep: int,
+                 ko_pos = None  # 根节点劫争位
+                 ):
         self.sg_selector = GoStrategySelector()  # 策略选择器
         self.va_predictor = GoValuePredictor()   # 价值选择器
         self.va_engine = KataGoEngine()          # katago价值判断器
-        self.root_state = root_state.copy()
+        self.root_state = root_state.copy()      # 根节点棋盘状态
         self.root_player = root_player           # 根节点行棋方
         self.opp_player = 'b' if self.root_player == 'w' else 'w'
         self.cursteps = curstep                  # 双方走的步数
         self.steps_list = steps_list             # 双方当前局面落子记录
-        self.root = None
-        self.leaves = []
+        self.root = None                         # 搜索树根节点
+        self.ko_pos = ko_pos                     # 根节点劫争位
+        self.leaves = []                         # 所有叶节点列表
 
     def build_tree(self):
         '''从根节点开始按照MAX_SEARCH_DEEPTH构建搜索树'''
-        self.root = TreeNode(self.root_state, self.root_player, deepth=0, is_root=True, root_moves=self.steps_list)
+        self.root = TreeNode(
+            self.root_state, 
+            self.root_player, 
+            ko_pos=self.ko_pos,
+            deepth=0, 
+            is_root=True, 
+            root_moves=self.steps_list
+        )
         queue = deque([self.root])
+
+        def _is_legal(pos, node: TreeNode):
+            '''
+            判断该情况下落子是否合法
+                落子位置是上一次劫争位
+                落子位置已经有棋子
+                落子位置导致自己棋子被提
+            '''
+            pos = tuple(pos)
+            # 判断是否已经有棋子
+            if node.board_state.get(pos[0], pos[1]) is not None:
+                return False
+            
+            # 判断是否上一次劫争位
+            # print(f'判断合法性: 落子 {pos} | 当前节点劫争位: {node.ko_pos}')
+            if node.ko_pos is not None and pos == node.ko_pos:
+                return False
+            
+            # 判断落子后是否自己棋子被提
+            test_board = node.board_state.copy()
+            _, to_be_captured = test_board.play(pos[0], pos[1], node.cur_player)
+            if pos in to_be_captured:
+                return False
+
+            return True
 
         while queue:
             node: TreeNode = queue.popleft()
@@ -191,11 +233,15 @@ class MinimaxMCR:
             for move, prob in topk_moves:
                 if move == 'pass':
                     continue
-                node.board_state.play(move[0], move[1], node.cur_player)
+                if not _is_legal(move, node):
+                    continue
+                child_board = node.board_state.copy()
+                ko_pos, _ = child_board.play(move[0], move[1], node.cur_player)
                 new_player = 'b' if node.cur_player == 'w' else 'w'
                 child = TreeNode(
-                    node.board_state,
+                    child_board,
                     new_player,
+                    ko_pos=ko_pos,
                     deepth=node.deepth + 1,
                     parent=node,
                     by_move=move
@@ -228,6 +274,7 @@ class MinimaxMCR:
     
     def rollout_leaf_kata(self, leaf_node: TreeNode):
         '''使用katago计算叶节点的局面价值'''
+        print(f'叶节点: {leaf_node} | 落子序列: {leaf_node.moves}')
         return self.va_engine.get_value(self.root_player, leaf_node.moves)
     
     def evaluate_all_leaves(self):
@@ -286,6 +333,7 @@ class GoPlayer:
 
         self.curstep = 0
         self.steps = []  # 记录落子信息
+        self.ko_pos = None # 记录劫争位
 
     def play_move_str(self, go_str:str):
         """执行落子 字符坐标"""
@@ -295,7 +343,7 @@ class GoPlayer:
             pos = go_str_to_idx(go_str)
             self.last_board = self.board.copy()
             self.last_color = self.current_color
-            self.board.play(pos[0], pos[1], self.current_color)
+            self.ko_pos, _ = self.board.play(pos[0], pos[1], self.current_color)
             print(f"{self.current_color.upper()} 落子：({pos[0]}, {pos[1]})")
             self.steps.append([self.current_color.upper(), idx_to_go_str((pos[0], pos[1]), False)])
         
@@ -304,8 +352,8 @@ class GoPlayer:
         self.curstep += 1
     
     def play_move(self, go_pos:tuple):
-        '''执行落子 元组坐标'''
-        self.board.play(go_pos[0], go_pos[1], self.current_color)
+        '''执行落子 元组坐标, 返回劫争位'''
+        self.ko_pos, _ = self.board.play(go_pos[0], go_pos[1], self.current_color)
         self.steps.append([self.current_color.upper(), idx_to_go_str((go_pos[0], go_pos[1]), False)])
         self.current_color = 'w' if self.current_color == 'b' else 'b'
         self.curstep += 1
@@ -377,13 +425,26 @@ class GoBoardDector:
             cv2.THRESH_BINARY
         )
 
-        # self.show_board_img('白棋二值化特征图' ,w_thresh, 0)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+        w_thresh = cv2.morphologyEx(w_thresh, cv2.MORPH_CLOSE, kernel)
+
+        
 
         for r in range(self.board_size):
             for c in range(self.board_size):
-                pix_pos = (META_ANALYSYS_POS[0] + r * GAP - WHITE_BIAS, META_ANALYSYS_POS[1] + c * GAP - WHITE_BIAS)
-                if w_thresh[pix_pos[0], pix_pos[1]] == 255:
+                # 区域采样
+                roi_ok = False
+                roi = w_thresh[
+                    META_ANALYSYS_POS[0] + r * GAP - WHITE_BIAS : META_ANALYSYS_POS[0] + r * GAP + WHITE_BIAS,
+                    META_ANALYSYS_POS[1] + c * GAP - WHITE_BIAS : META_ANALYSYS_POS[1] + c * GAP + WHITE_BIAS
+                ]
+                roi_ok = np.any(roi == 255)
+                if roi_ok:
                     self.cur_board_list.append((18 - r, c))
+                    # go_str = idx_to_go_str((18 - r, c), False)
+                    # print(f"【识别】白棋：{go_str} (r={r}, c={c})")
+        
+        # self.show_board_img('白棋二值化特征图' ,w_thresh, 0)
     
     def analyze_cur_board(self):
         '''对棋盘截图, 分析得到当前棋盘状态'''
@@ -405,9 +466,9 @@ class GoBoardDector:
                 return pos
         return None
 
-def ai_play(steps_list, board: boards.Board, color, curstep):
+def ai_play(steps_list, board: boards.Board, color, curstep, ko_pos=None):
     '''ai行棋'''
-    minimax_search = MinimaxMCR(steps_list, board, color, curstep)
+    minimax_search = MinimaxMCR(steps_list, board, color, curstep, ko_pos)
     if curstep % 10 == 0:  # 每10手重启一次katago
         minimax_search.va_engine.restart()
     best_move, best_value = minimax_search.search()
@@ -419,7 +480,13 @@ def vs_ai():
     print("* 落子输入格式：列字符+行坐标 J8 ")
     
     if go_player.ai_player == 'b':  # 若黑 ai先下
-        move = ai_play(go_player.steps, go_player.board, 'b', go_player.curstep)
+        move = ai_play(
+            go_player.steps, 
+            go_player.board, 
+            'b', 
+            go_player.curstep, 
+            ko_pos=None
+        )
         go_player.play_move(move)
 
     while True:
@@ -433,10 +500,16 @@ def vs_ai():
             continue
         else:
             go_player.play_move_str(human_input)
-            print(go_player.steps)
+            print(f'当前落子序列: {go_player.steps}')
         
         # AI落子
-        ai_move = ai_play(go_player.steps, go_player.board, go_player.current_color, go_player.curstep)
+        ai_move = ai_play(
+            go_player.steps, 
+            go_player.board, 
+            go_player.current_color, 
+            go_player.curstep, 
+            go_player.ko_pos
+        )
         if ai_move == 'pass':
             print("AI选择弃行")
             go_player.play_move_str('pass')
@@ -444,13 +517,19 @@ def vs_ai():
             go_player.play_move(ai_move)
 
 def vs_ai_auto():
-    go_player = GoPlayer('b')
+    go_player = GoPlayer('w')
     board_dector = GoBoardDector()
     board_dector.analyze_cur_board()
     print('模型加载完成')
 
-    if go_player.ai_player == 'w':
-        move = ai_play(go_player.steps, go_player.board, 'b', go_player.curstep)
+    if go_player.ai_player == 'b':
+        move = ai_play(
+            go_player.steps, 
+            go_player.board, 
+            'b', 
+            go_player.curstep, 
+            ko_pos=None
+        )
         go_player.play_move(move)
         board_dector.do_move(move)
 
@@ -459,20 +538,24 @@ def vs_ai_auto():
         board_dector.analyze_cur_board()
         new_stone_pos = board_dector.cal_new_stone_pos()
         if new_stone_pos is None:
-            time.sleep(1)
+            time.sleep(0.5)
             continue
 
-        print(f'新落子: {idx_to_go_str(new_stone_pos)}')
+        print(f'\n新落子: {idx_to_go_str(new_stone_pos)}')
         go_player.play_move_str(idx_to_go_str(new_stone_pos))
         go_player.print_board()
-        print(go_player.steps)
+        print(f'当前落子序列: {go_player.steps}')
 
         # AI落子
-        ai_move = ai_play(go_player.steps, go_player.board, go_player.current_color, go_player.curstep)
+        ai_move = ai_play(
+            go_player.steps, 
+            go_player.board, 
+            go_player.current_color, 
+            go_player.curstep,
+            go_player.ko_pos
+        )
         go_player.play_move(ai_move)
         board_dector.do_move(ai_move)
-
-
 
 if __name__ == "__main__":
     vs_ai_auto()
